5814c19b8a92309c77ad2a12ec2884c616fd6e21
yuyidong <378040621@qq.com>
2016-01-26 15:45:04 +0800

    delete test

--- app/src/main/java/com/yydcdut/demo/NormalActivity.java
+++ app/src/main/java/com/yydcdut/demo/NormalActivity.java
@@ -7,8 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.Transformation;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -43,39 +41,6 @@ public class NormalActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
                 Log.i(TAG, "onItemClick   position--->" + position);
                 Toast.makeText(NormalActivity.this, "onItemClick   position--->" + position, Toast.LENGTH_SHORT).show();
-//                final View viewTop = listView.getChildAt(position - listView.getFirstVisiblePosition());
-//                Log.i(TAG, "" + viewTop.getLeft() + "  " + viewTop.getTop() + "  " + viewTop.getRight() + "  " + viewTop.getBottom());
-//                final View viewBottom = listView.getChildAt(position - listView.getFirstVisiblePosition() + 1);
-//                Log.i(TAG, "" + viewBottom.getLeft() + "  " + viewBottom.getTop() + "  " + viewBottom.getRight() + "  " + viewBottom.getBottom());
-//                final int delta = viewBottom.getTop() - viewTop.getTop();
-////                UDAnimation animation = new UDAnimation(delta, viewTop, viewBottom);
-////                animation.start();
-//                ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
-//                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//                    @Override
-//                    public void onAnimationUpdate(ValueAnimator animation) {
-//                        float disF = delta * ((Float) animation.getAnimatedValue());
-//                        viewTop.setTranslationY(disF);
-//                        viewBottom.setTranslationY(-disF);
-//                        viewTop.invalidate();
-//                        viewBottom.invalidate();
-//                    }
-//
-//                });
-//                animator.addListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationEnd(Animator animation) {
-//                        super.onAnimationEnd(animation);
-//                        viewTop.setTranslationY(0);
-//                        viewBottom.setTranslationY(0);
-//                        ApplicationInfo applicationInfo = mAppList.get(position - listView.getHeaderViewsCount());
-//                        mAppList.set(position - listView.getHeaderViewsCount(), mAppList.get(position + 1 - listView.getHeaderViewsCount()));
-//                        mAppList.set(position + 1 - listView.getHeaderViewsCount(), applicationInfo);
-//                        mAdapter.notifyDataSetChanged();
-//                    }
-//                });
-//                animator.setDuration(250).start();
-
             }
         });
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -86,32 +51,6 @@ public class NormalActivity extends AppCompatActivity {
                 return false;
             }
         });
-    }
-
-    class UDAnimation extends Animation {
-        private int dis;
-        private View upView;
-        private View downView;
-
-        public UDAnimation(int dis, View upView, View downView) {
-            Log.i(TAG, "UDAnimationUDAnimationUDAnimation");
-            this.dis = dis;
-            this.upView = upView;
-            this.downView = downView;
-            setDuration(1000);
-            start();
-        }
-
-        @Override
-        protected void applyTransformation(float interpolatedTime, Transformation t) {
-            float disF = dis * interpolatedTime;
-            Log.i(TAG, "" + disF);
-            downView.setTranslationY(disF);
-            upView.setTranslationY(-disF);
-            downView.invalidate();
-            downView.requestLayout();
-            upView.invalidate();
-        }
     }
 
     private BaseAdapter mAdapter = new BaseAdapter() {
