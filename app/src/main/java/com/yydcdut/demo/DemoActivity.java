71dfc4f4b005a4565125d8d0cb47da56cee9df6d
yuyidong <378040621@qq.com>
2016-04-18 14:01:03 +0800

    add branch

--- app/src/main/java/com/yydcdut/demo/DemoActivity.java
+++ app/src/main/java/com/yydcdut/demo/DemoActivity.java
@@ -48,6 +48,7 @@ public class DemoActivity extends AppCompatActivity implements SlideAndDragListV
     }
 
     public void initMenu() {
+
         mMenu = new Menu(new ColorDrawable(Color.LTGRAY), true);
         mMenu.addItem(new MenuItem.Builder().setWidth((int) getResources().getDimension(R.dimen.slv_item_bg_btn_width) * 2)
                 .setBackground(new ColorDrawable(Color.RED))
