898261412e936bee1cc69717ea561737bfe1b676
yuyidong <378040621@qq.com>
2017-05-31 10:03:11 +0800

    Add license

--- sdlv/src/main/java/com/yydcdut/sdlv/Compat.java
+++ sdlv/src/main/java/com/yydcdut/sdlv/Compat.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 yydcdut (yuyidong2015@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.yydcdut.sdlv;
 
 import android.graphics.drawable.Drawable;
@@ -8,7 +23,6 @@ import android.view.View;
  * Created by yuyidong on 15/12/7.
  */
 class Compat {
-
     protected static void setBackgroundDrawable(View view, Drawable drawable) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             view.setBackground(drawable);
